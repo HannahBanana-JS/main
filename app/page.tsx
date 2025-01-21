71b31b3d6733eb271470cad965f9e0119e776013
HannahBanana-JS <86928611+HannahBanana-JS@users.noreply.github.com>
2025-01-21 12:34:01 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(outputs);
+
+const client = generateClient<Schema>();
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  function listTodos() {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    });
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+    });
+  }
+
+  return (
+    <main>
+     export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Custom Website</h1>
+      <p>This is the homepage of my website, built with AWS Amplify and Next.js!</p>
+    </div>
+  );
+}
+
+      <div>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+          Review next steps of this tutorial.
+        </a>
+      </div>
+    </main>
+  );
+}
